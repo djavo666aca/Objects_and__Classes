f1f7cf6a1c70f6d3de3fe6865f67b897c97ceebe
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-07 02:55:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System; 
+
+namespace Objects_and__Classes
+{
+    internal class Program
+    {
+        string x = "Genericka vrednost x";
+        string y = "Genericka vrednost y";
+
+        public static void Metoda() 
+        {
+        Program TreciObjekat = new Program();
+            Console.WriteLine(TreciObjekat.x);
+            Console.WriteLine(TreciObjekat.y);
+        }
+
+        static void Main(string[] args)
+        {
+            Program PrviObjekat = new Program();
+            Console.WriteLine(PrviObjekat.x);
+            Console.WriteLine(PrviObjekat.y);
+
+            Program DrugiObjekat = new Program();
+            Console.WriteLine(PrviObjekat.x);
+            Console.WriteLine(PrviObjekat.y);
+
+            Console.WriteLine("######################");
+            Metoda();
+            
+        }
+    }
+}
